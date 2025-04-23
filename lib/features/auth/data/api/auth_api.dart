2094b9762dc11d0b74726222d0a381397a5b799f
Anna <anna.gudkova.rus@mail.ru>
2025-04-23 15:55:36 +0400

    auth store

--- lib/features/auth/data/api/auth_api.dart
+++ lib/features/auth/data/api/auth_api.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:jwt_decoder/jwt_decoder.dart';
 import '../../../core/constants/app_constants.dart';
+import '../entities/User.dart';
 import '../entities/auth_response.dart';
 import '../entities/login_request.dart';
 import '../entities/register_request.dart';
@@ -18,9 +20,22 @@ class AuthApi {
     );
 
     if (response.statusCode == 200) {
-      return AuthResponse.fromJson(json.decode(response.body));
+      final responseData = json.decode(response.body);
+      final token = responseData['token'];
+      final userData = responseData['user'];
+
+      // Проверка, что ID в JWT = ID в user объекте
+      final jwtUserId = _getUserIdFromJwt(token);
+      if (jwtUserId != userData['id']) {
+        throw Exception('User ID in JWT does not match user object');
+      }
+
+      return AuthResponse(
+        token: token,
+        user: User.fromJson(userData),
+      );
     } else {
-      throw Exception('Failed to login');
+      throw Exception('Failed to login: ${response.body}');
     }
   }
 
@@ -32,19 +47,45 @@ class AuthApi {
     );
 
     if (response.statusCode == 201) {
-      return AuthResponse.fromJson(json.decode(response.body));
+      final responseData = json.decode(response.body);
+      final token = responseData['token'];
+      final userData = responseData['user'];
+
+      // Проверка, что ID в JWT = ID в user объекте
+      final jwtUserId = _getUserIdFromJwt(token);
+      if (jwtUserId != userData['id']) {
+        throw Exception('User ID in JWT does not match user object');
+      }
+
+      return AuthResponse(
+        token: token,
+        user: User.fromJson(userData),
+      );
     } else {
-      throw Exception('Failed to register');
+      throw Exception('Failed to register: ${response.body}');
     }
   }
 
   Future<void> logout(String token) async {
-    await client.post(
+    final response = await client.post(
       Uri.parse('${ApiConstants.baseUrl}/auth/logout'),
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer $token',
       },
     );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to logout: ${response.body}');
+    }
+  }
+
+  String _getUserIdFromJwt(String jwt) {
+    try {
+      final decoded = JwtDecoder.decode(jwt);
+      return decoded['sub'] ?? decoded['userId']; // Поменять в зависимости от того, как бекенд хранит ID в токене
+    } catch (e) {
+      throw Exception('Failed to decode JWT: $e');
+    }
   }
 }
